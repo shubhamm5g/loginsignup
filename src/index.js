1f40f2d2d148f4bc7fc8891b78357120206e7902
Shubham Maurya <shubhammaurya5g@gmail.com>
2023-02-05 15:01:45 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const ejs=require("ejs")
+const { join } = require('path')
+
+const collection=require('./mongodb')
+
+const tempeletePath=path.join(__dirname,'../tempelates')
+
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'../public')))
+app.use(express.json())
+app.set("view engine","ejs")
+app.set('views',tempeletePath)
+
+app.get('/',(req,res)=>{
+    res.render('login')
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('signup')
+})
+
+app.post('/signup',async (req,res)=>{
+    const data={
+        username:req.body.name,
+        password:req.body.password,
+        contactno:req.body.contact,
+        flatno:req.body.flatno,
+        wingname:req.body.wingname,
+        fullname:req.body.fullname
+    }
+
+    await collection.insertMany([data])
+
+    res.render('login')
+})
+
+app.post('/login',async (req,res)=>{
+
+    try{
+        const check=await collection.findOne({name:req.body.name,password:req.body.password})
+
+        if(check.password===req.body.password && check.name==req.body.name){
+            res.render("home")
+        }
+        else{
+            res.send("worng password")
+        }
+    }
+    catch{
+        res.send("wrong detailes")
+
+    }
+
+})
+
+
+app.listen(process.env.PORT || 2000)
---
+++ src/mongodb.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb://localhost:27017/loginInformation')
+.then(()=>{
+    console.log("mongo connected");
+})
+.catch(()=>{
+    console.log('failed to connect')
+})
+
+const LogInSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    contactno:{
+        type:String,
+        required:true
+    },
+    flatno:{
+        type:String,
+        required:true
+    },
+    wingname:{
+        type:String,
+        required:true
+    },
+    fullname:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = new mongoose.model('Basic Members Information',LogInSchema)
+
+module.exports=collection
